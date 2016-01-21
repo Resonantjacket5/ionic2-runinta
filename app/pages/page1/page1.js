abef6091c77d8720f15e308f9d27005cff85ab25
Wesley Lin <wesley.lin@vanderbilt.edu>
2016-01-21 13:05:37 +0100

    reenabled google map api on index.html page. wrote map code for the page1 (map tab)

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -6,6 +6,21 @@ import {Page} from 'ionic/ionic';
 })
 export class Page1 {
   constructor() {
+    var latlng = {
+      //Copenhagen coordinates :) 
+      lat:55.6761,
+      lng:12.5683
+    };
+    
+    var mapOptions = {
+      center: latlng,
+      zoom: 15//, 
+      //mapTypeId: google.map.MapTypeId.ROADMAP
 
+    };
+    
+    console.log("try to fetch map!");
+    this.map = new google.maps.Map(document.getElementById("map"),mapOptions);
+    
   }
 }
