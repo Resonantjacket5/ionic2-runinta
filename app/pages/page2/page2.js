f327e384853eb7c9a3930d3cfa184f483be9d036
Wesley Lin <wesley.lin@vanderbilt.edu>
2016-02-18 12:34:31 +0100

    added fetching coordinates from friends

--- app/pages/page2/page2.js
+++ app/pages/page2/page2.js
@@ -11,23 +11,27 @@ export class Page2 {
   
   constructor(nav: NavController, auth: AuthService) {
     var _this =this;
-    
+    this.locationProvided = false;
     this.nav = nav;
     this.auth = auth;
     this.friends = [];
-    this.friends = [
-      {
-        name: "Sam",
-        age:  19
-      }
-      {
-        name: "Tom",
-        age: 25
-      }]
-    this.coordinates = {
-      latitude: 8,
-      longitude: 2
-    };
+    
+// default data used for debugging
+//    this.friends = [
+//      {
+//        name: "Sam",
+//        age:  19,
+//        coords: 1
+//      }
+//      {
+//        name: "Tom",
+//        age: 25,
+//        coords: 2
+//      }]
+//    this.coordinates = {
+//      latitude: 8,
+//      longitude: 2
+//    };
     
   };
   
@@ -66,12 +70,40 @@ export class Page2 {
     }, function(errorobject){
       console.log("The read failed: "+errorobject.code);
     });
+    
     //*/
+    
+    
   };
+
+
+  updateFriendsLocation() {
+    //let ref = this.auth.getFireBaseRef();
+                              
+    for(let i = 0; i<this.friends.length; i++){
+      
+      //this.friends[i].coords = ref.child('geofire').get(this.friends[i].uid);
+      this.fetchGeoFireFriend(i);
+      //ref.child('geofire').child('
+    }
+    
+    console.log(this.friends);
+  }
+
+  fetchGeoFireFriend(i){
+    let geoFire = new GeoFire(this.auth.getFireBaseRef().child("geofire");
+    geoFire.get(this.friends[i].uid).then( (location) => {
+      console.log("location fetched "+location);
+      this.friends[i].latitude = location[0];
+      this.friends[i].longitude = location[1];
+      console.log("finisiehd getting location");
+    }, function (error) {
+      console.log("No coordinates data: " +error); 
+    });
+  }
   
   askUserLocation = function () {
     navigator.geolocation.getCurrentPosition(this.onSuccess,this.onError);
-    this.pushLocation();
   };
                               
 
@@ -79,6 +111,11 @@ export class Page2 {
   onSuccess = function(position) {
     _this.coordinates.latitude= position.coords.latitude;
     _this.coordinates.longitude = position.coords.longitude;
+    _this.pushLocation();
+  };
+      
+  onError= function (error){
+    console.log("error"); 
   };
 
   // function called on success, from askUserLocation
@@ -93,18 +130,16 @@ export class Page2 {
     }, function (error) {
       console.log("Error: "+error);
     });
+ 
+  };
+  
+  
     /*
     geoFire.set("some_key", [37.785326, -122.405696]).then(function() {
       console.log("Provided key has been added to GeoFire");
     }, function(error) {
       console.log("Error: " + error);
     });//*/
-  };
-  
-  onError= function (error){
-    console.log("error"); 
-  };
-
 
 
   logOut = function () {
